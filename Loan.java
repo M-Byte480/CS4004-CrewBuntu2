8ae40f2dc6d90d86e8075a16c63d4944fd063323
Eamonn <21204616@studentmail.ul.ie>
2022-11-08 18:43:02 +0000

    Created basic structure of Loan Object

---
+++ Loan.java
@@ -0,0 +1,15 @@
+import java.utils.LocalDate;
+public class Loan {
+    private Student loanee;
+    private Book book;
+    private LocalDate startDate;
+    private LocalDate dueDate;
+    private static int loanLength = 7;
+
+    public Loan(Student loanee, Book book, LocalDate startDate){
+        this.loanee = loanee;
+        this.book = book;
+        this.startDate = startDate;
+        this.dueDate = startDate.plusDays(loanLength);
+    }
+}

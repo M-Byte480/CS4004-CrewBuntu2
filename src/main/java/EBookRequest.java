00be751e099feaedb6c1a06fc8919dcc8e43dc1c
Eamonn <21204616@studentmail.ul.ie>
2022-11-18 19:34:14 +0000

    - Added a journal variable for Ebook Requests for the journals that need to have an eBook edition ordered - Overloaded the EbookRequest constructor to handle a Journal parameter.

--- src/main/java/EBookRequest.java
+++ src/main/java/EBookRequest.java
@@ -3,11 +3,18 @@ import java.time.LocalDateTime;
 public class EBookRequest {
     private LocalDateTime timeOfRequest;
     private Book book;
+    private Journal journal;
     private boolean isCompleted;
 
     public EBookRequest(Book book){
         this.timeOfRequest = LocalDateTime.now();
         this.book = book;
+        this.isCompleted = false;
+    }
+
+    public EBookRequest(Journal journal){
+        this.timeOfRequest = LocalDateTime.now();
+        this.journal = journal;
         this.isCompleted = false;
     }
 }

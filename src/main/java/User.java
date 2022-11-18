2887e92050acc771db498d7494872a5f6918ad70
M-Byte480 <77645654+M-Byte480@users.noreply.github.com>
2022-11-18 23:14:59 +0000

    Updated to make everything united

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -13,25 +13,6 @@ public class User {
         this.email = email;
     }
 
-    public String getUsername() { return username; }
-
-    public void setUsername(String username) { this.username = username; }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 
     public static boolean checkValidUsername(String username) { return (username.trim().length() >= 8 && username.trim().length() <= 20); }
 
--- src/test/java/JacobUnitLibraryTests.java
+++ src/test/java/JacobUnitLibraryTests.java
@@ -643,4 +643,33 @@ go
     }
 
     // ===================================== End of Aaron =========================================== //
+    // ====================================== Eamonn ================================================ //
+    @Test
+
+    public void isNewEBookRequestMadeWhenNotEJournal(){
+        Journal journal1 = new Journal("journal1","author1");
+
+        int sizeBefore = Library.getRequestsForEBooks().size();
+        journal1.beingBound();
+        int sizeAfter = Library.getRequestsForEBooks().size();
+        assertTrue(sizeBefore + 1 == sizeAfter);
+
+        Journal journal2 = new Journal("journal2","author2");
+        sizeBefore = Library.getRequestsForEBooks().size();
+        journal1.beingBound();
+        sizeAfter = Library.getRequestsForEBooks().size();
+        assertTrue(sizeBefore + 1 == sizeAfter);
+    }
+
+    @Test
+    public void isNewEBookRequestMadeWhenEJournalConstructor(){
+        Journal journal1 = new Journal("journal1",true);
+
+        int sizeBefore = Library.getRequestsForEBooks().size();
+        journal1.beingBound();
+        int sizeAfter = Library.getRequestsForEBooks().size();
+        assertTrue(sizeBefore == sizeAfter);
+
+    }
+    // =============================================== End of Eamonn ===================================== //
 }
